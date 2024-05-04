ecbcf345ff68ab2019a2cd01dad258483c7877cd
rabiasafak <rabiasafak364@gmail.com>
2024-05-04 16:22:26 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Sticky Navbar
+window.addEventListener("scroll", function () {
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 100);
+  });
+  
+  // Show scrool btn when scroll down
+  window.addEventListener("scroll", function () {
+    let scrool = document.querySelector("#scrool-up");
+    scrool.classList.toggle("srcl", window.scrollY > 600);
+  });
+  
